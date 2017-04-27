2b7df0c78a196a4c8f46c9b53e43bbc536a44f3d
iroha168 <cnoizecehiky@yahoo.co.jp>
2017-04-28 05:11:19 +0900

    now selected problems can be passed to contest/register.html

--- src/main/java/crooms/app/contest/ContestController.java
+++ src/main/java/crooms/app/contest/ContestController.java
@@ -1,5 +1,6 @@
 package crooms.app.contest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -7,9 +8,12 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import crooms.app.problem.SelectedProblemsForm;
 import crooms.domain.model.problem.CodeforcesProblems;
 import crooms.domain.repository.problem.ProblemRepository;
 import crooms.domain.service.Problem.ProblemService;
@@ -23,10 +27,23 @@ public class ContestController {
 	public String listContest(){
 		return "contest/list";
 	}
+	@ModelAttribute
+	private ContestForm setUpForm(){
+		return new ContestForm();
+	}
 	
-	@RequestMapping(path = "/register")
-	public String registerContent(Model model){
-		model.addAttribute(new ContestForm());
+	@RequestMapping(path = "/register", method = RequestMethod.GET)
+	public String registerContest(Model model){
+		return "contest/register";
+	}
+
+	@RequestMapping(path = "/register", method = RequestMethod.POST)
+	public String registerContent(@ModelAttribute("selectedProblemsForm") SelectedProblemsForm form, Model model){
+		List<String> ids = new ArrayList<>();
+		for(String id : form.getCkId()){
+			ids.add(id);
+		}
+		model.addAttribute("ids", ids);
 		return "contest/register";
 	}
 }
